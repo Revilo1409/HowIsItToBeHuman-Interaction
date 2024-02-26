c3acde3226a6b5bb2f2463b37204927457d3511e
Oliver Aberle <Oliver.Aberle@student.kit.edu>
2024-02-26 16:09:26 +0100

    Changed the api_key to be a parameter. added a few comments

--- sarai_chatgpt/gpt_requester_node.py
+++ sarai_chatgpt/gpt_requester_node.py
@@ -19,23 +19,43 @@ class GPTRequester(Node):
 
     def __init__(self):
         super().__init__('gptrequester')
+        # Service for sending a text request to the GPT API
         self.gpt_request_srv = self.create_service(GPTRequest, 'gpt_request', self.gpt_request_callback) 
+
         role_descriptor = ParameterDescriptor(description = """This is a 
                             parameter for setting the role of ChatGPT""") 
         maxWindow_messages_descriptor = ParameterDescriptor(description = "Max number of messages stored in the message history") 
-        api_key_descriptor = ParameterDescriptor(description= "API Key for ChatGPT API")                                      
-        self.declare_parameter('role', '', role_descriptor)
+        api_key_descriptor = ParameterDescriptor(description= "API Key for ChatGPT API") 
+        
+        #Parameter for setting the role of ChatGPT                                     
+        self.declare_parameter('role', 'None', role_descriptor)
+        # Parameter for setting the maximum number of messages in the history that should be sent to GPT
         self.declare_parameter('maxWindow_messages', 100, maxWindow_messages_descriptor)
-        self.api_key = sys.argv[1]                               
-        self.client = OpenAI(api_key = self.api_key)
+        # API Key for ChatGPT API
+        self.declare_parameter('api_key', 'None', api_key_descriptor)
+
+        self.client = OpenAI(api_key = self.get_parameter('api_key').get_parameter_value().string_value)
 
     def gpt_request_callback(self, request, response):
+        """
+        Service handler performing a request to the GPT API
+
+        :param request: See GPTRequest service definition.
+        :param response: See GPTRequest service definition
+        """
         chat = self.client.chat.completions.create(model=self.MODEL, messages= self.get_maxWindow_messages(request.user_input),temperature=0.7)
         response.chatgpt_response = chat.choices[0].message.content
         response.success = True
         return response  
         
     def get_maxWindow_messages(self, user_input):
+        """
+        Help method that appends role_message, last maxWindow_messages of 
+        message history and the user_input
+        :param user_input: Message the user wants to send to GPT API
+        :return:  role_message, last maxWindow_messages of 
+                message history and the user_input appended together
+        """
         role_message = {"role": "system", "content": self.get_parameter('role').get_parameter_value().string_value}
         maxWindow_messages = self.get_parameter('maxWindow_messages').get_parameter_value().integer_value
         self.MESSAGE_HISTORY.append({"role": "user", "content" : user_input})
