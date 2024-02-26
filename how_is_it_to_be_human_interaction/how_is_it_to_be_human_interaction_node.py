0f9786f05be9c55b19df5eaedbfa17570f0aa8b6
Oliver Aberle <Oliver.Aberle@student.kit.edu>
2024-02-26 14:56:43 +0100

    Created new client and method for speech recognition

--- how_is_it_to_be_human_interaction/how_is_it_to_be_human_interaction_node.py
+++ how_is_it_to_be_human_interaction/how_is_it_to_be_human_interaction_node.py
@@ -1,9 +1,7 @@
 import rclpy
 from rclpy.node import Node
 
-from sarai_msgs.srv import SetSpeech
-
-from sarai_msgs.srv import GPTRequest
+from sarai_msgs.srv import SetSpeech, GPTRequest, RecognizeSpeech
 
 class Interaction(Node):
     def __init__(self):
@@ -13,17 +11,19 @@ class Interaction(Node):
 
         self.speak_cli = self.create_client(SetSpeech, 'speak')
 
+        self.recognize_speech_cli = self.create_client(RecognizeSpeech, 'recognize_speech')
+
         while not self.gpt_request_cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info(f'{self.gpt_request_cli.srv_name} service not available, waiting again...')
     
-    def gpt_request(self, user_input):
+    def send_gpt_request(self, user_input):
         request = GPTRequest.Request()
         request.user_input = user_input
         self.future = self.gpt_request_cli.call_async(request)
         rclpy.spin_until_future_complete(self, self.future)
         return self.future.result()
     
-    def speak(self, gpt_response):
+    def send_speak_request(self, gpt_response):
         request = SetSpeech.Request()
 
         print(f"typ: {type(gpt_response)}")
@@ -35,6 +35,12 @@ class Interaction(Node):
 
         return self.future.result()
     
+    def send_recognize_speech_request(self):
+        request = RecognizeSpeech.Request()
+        self.future = self.recognize_speech_cli.call_async(request)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
+    
     def interaction(self):
         while True:
             try:
@@ -42,8 +48,8 @@ class Interaction(Node):
             except KeyboardInterrupt:
                 break
             if message:
-                response = self.gpt_request(message)
-                self.speak(response.chatgpt_response)
+                response = self.send_gpt_request(message)
+                self.send_speak_request(response.chatgpt_response)
                 print(f"GPT: {response.chatgpt_response}")
 
         print("\nClosing GPTClient...")
