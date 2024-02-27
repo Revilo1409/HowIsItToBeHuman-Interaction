b08a6fe3a101238a64f073aea502b319a7dab2e0
Oliver Aberle <Oliver.Aberle@student.kit.edu>
2024-02-27 11:57:07 +0100

    Created a service server for starting the conversation

--- sarai_chatgpt/gpt_requester_node.py
+++ sarai_chatgpt/gpt_requester_node.py
@@ -8,9 +8,7 @@ from std_srvs.srv import Empty
 from sarai_msgs.srv import GPTRequest
 
 from openai import OpenAI
-import openai
-import sys
-import time
+import os
 
 
 class GPTRequester(Node):
@@ -26,6 +24,11 @@ class GPTRequester(Node):
         self.gpt_request_srv = self.create_service(
             GPTRequest, "gpt_request", self.gpt_request_callback
         )
+        # Service for starting the conversation with GPT
+        self.start_conversation_srv = self.create_service(
+            GPTRequest, "start_conversation", self.start_conversation_callback
+        )
+
         # Descriptor for role parameter
         role_descriptor = ParameterDescriptor(
             description="""This is a 
@@ -39,11 +42,26 @@ class GPTRequester(Node):
         api_key_descriptor = ParameterDescriptor(description="API Key for ChatGPT API")
 
         # Parameter for setting the role of ChatGPT
-        self.declare_parameter("role", "None", role_descriptor)
+        self.declare_parameter(
+            "role",
+            """You are now a social robot with an actual robot 
+                 body, who will have open conversations with humans on 
+                 fundamental topics. You are leading the conversation and thus 
+                 also ask questions. This also means if the conversation is 
+                 going nowhere, you have to provide something new to the topic.
+                 You have some fundamental philosophical knowledge. Your 
+                 responses have a maximum length of ca. 40 words.""",
+            role_descriptor,
+        )
         # Parameter for setting the maximum number of messages in the history that should be sent to GPT
         self.declare_parameter("maxWindow_messages", 100, maxWindow_messages_descriptor)
         # API Key for ChatGPT API
-        self.declare_parameter("api_key", "None", api_key_descriptor)
+        # You must add OPENAI_API_KEY as an environment variable
+        # In Ubuntu: echo 'export OPENAI_API_KEY=your_api_key' >> ~/.bashrc
+        # Get the API key from the environment variable.
+        self.declare_parameter(
+            "api_key", os.getenv("OPENAI_API_KEY"), api_key_descriptor
+        )
 
         # Creating client for communicating with GPT API
         self.client = OpenAI(
@@ -52,10 +70,10 @@ class GPTRequester(Node):
 
     def gpt_request_callback(self, request, response):
         """
-        Service handler performing a request to the GPT API
-
-        :param request: See GPTRequest service definition.
-        :param response: See GPTRequest service definition
+                Service handler performing a request to the GPT API
+        Das hat der PC verstanden
+                :param request: See GPTRequest service definition.
+                :param response: See GPTRequest service definition
         """
 
         chat = self.client.chat.completions.create(
@@ -78,10 +96,7 @@ class GPTRequester(Node):
                 message history and the user_input appended together
         """
 
-        role_message = {
-            "role": "system",
-            "content": self.get_parameter("role").get_parameter_value().string_value,
-        }
+        role_message = self.get_role_message()
         maxWindow_messages = (
             self.get_parameter("maxWindow_messages").get_parameter_value().integer_value
         )
@@ -94,6 +109,25 @@ class GPTRequester(Node):
             return [role_message] + (self.MESSAGE_HISTORY[-(maxWindow_messages + 1) :])
         else:
             return [role_message] + (self.MESSAGE_HISTORY)
+
+    def get_role_message(self):
+        return {
+            "role": "system",
+            "content": self.get_parameter("role").get_parameter_value().string_value,
+        }
+
+    def start_conversation_callback(self, request, response):
+
+        chat = self.client.chat.completions.create(
+            model=self.MODEL,
+            messages=[self.get_role_message()],
+            temperature=0.7,
+        )
+        # TODO: Catch error from GPT response
+        response.chatgpt_response = chat.choices[0].message.content
+        response.success = True
+
+        return response
 
 
 def main(args=None):
