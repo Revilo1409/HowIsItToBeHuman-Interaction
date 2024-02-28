f0ed6b2e990da4fb9ae213c5cf8a09d1839fa315
Oliver Aberle <Oliver.Aberle@student.kit.edu>
2024-02-28 11:15:21 +0100

    Removed service and method for starting a conversation. Instead i added a variable for that and an if statement in the gpt_request method

--- sarai_chatgpt/gpt_requester_node.py
+++ sarai_chatgpt/gpt_requester_node.py
@@ -15,36 +15,34 @@ class GPTRequester(Node):
     MODEL = "gpt-3.5-turbo-1106"
 
     # Parameter for storing the message history with GPT
-    MESSAGE_HISTORY = []
+    message_history = []
+
+    # Boolean that states if the conversation already has been started before
+    conversation_started = False
 
     def __init__(self):
         super().__init__("gptrequester")
 
         # Service for sending a text request to the GPT API
         self.gpt_request_srv = self.create_service(GPTRequest, "gpt_request", self.gpt_request_callback)
-        # Service for starting the conversation with GPT
-        self.start_conversation_srv = self.create_service(GPTRequest, "start_conversation", self.start_conversation_callback)
 
         # Descriptor for role parameter
-        role_descriptor = ParameterDescriptor(description="This is a parameter for setting the role of ChatGPT")
+        chatgpt_persona_descriptor = ParameterDescriptor(description="This is a parameter for setting the role of ChatGPT")
         # Descriptor for maxWindow_messages parameter
         max_window_messages_descriptor = ParameterDescriptor(description="Max number of messages stored in the message history")
         # Descriptor for api_key parameter
         api_key_descriptor = ParameterDescriptor(description="API Key for ChatGPT API")
 
         # Parameter for setting the role of ChatGPT
-        default_role = """You are now a social robot with an actual robot 
+        default_chatgpt_persona = """You are now a social robot with an actual robot 
                  body, who will have open conversations with humans on 
                  fundamental topics. You are leading the conversation and thus 
                  also ask questions. This also means if the conversation is 
                  going nowhere, you have to provide something new to the topic.
                  You have some fundamental philosophical knowledge. Your 
                  responses have a maximum length of ca. 40 words."""
-        self.declare_parameter(
-            "role",
-            default_role,
-            role_descriptor,
-        )
+        
+        self.declare_parameter("chatgpt_persona", default_chatgpt_persona, chatgpt_persona_descriptor,)
 
         # Parameter for setting the maximum number of messages in the history that should be sent to GPT
         self.declare_parameter("max_window_messages", 100, max_window_messages_descriptor)
@@ -67,10 +65,15 @@ class GPTRequester(Node):
 
         # Value between 0 and 1. Used to set the creativity of ChatGPTs answers
         temperature = 0.7
+        
+        messages= self.get_max_window_messages(request.user_input)
+
+        if not(self.conversation_started):
+            messages = [self.get_chatgpt_persona_message()]
 
         chat = self.gpt_client.chat.completions.create(
             model=self.MODEL,
-            messages=self.get_max_window_messages(request.user_input),
+            messages=messages,
             temperature=temperature,
         )
 
@@ -90,46 +93,33 @@ class GPTRequester(Node):
                 message history and the user_input appended together
         """
 
-        role_message = self.get_role_message()
+        role_message = self.get_chatgpt_persona_message()
         max_window_messages = (
             self.get_parameter("max_window_messages").get_parameter_value().integer_value
         )
 
         # Appends the current user input to the message history
-        self.MESSAGE_HISTORY.append({"role": "user", "content": user_input})
+        self.message_history.append({"role": "user", "content": user_input})
 
         # Returns the role message, and max. the last 4 messages of the message history + the current user input
-        if len(self.MESSAGE_HISTORY) > max_window_messages + 1:
-            return [role_message] + (self.MESSAGE_HISTORY[-(max_window_messages + 1) :])
+        if len(self.message_history) > max_window_messages + 1:
+            return [role_message] + (self.message_history[-(max_window_messages + 1) :])
         else:
-            return [role_message] + (self.MESSAGE_HISTORY)
+            return [role_message] + (self.message_history)
 
-    def get_role_message(self):
+    def get_chatgpt_persona_message(self):
         """
         Returns the formatted role message by using the role parameter.
 
         :return: Dict containing the role message.
         """
-        role_message = {
+        chatgpt_persona_message = {
             "role": "system",
-            "content": self.get_parameter("role").get_parameter_value().string_value,
+            "content": self.get_parameter("chatgpt_persona").get_parameter_value().string_value,
         }
 
-        return role_message
-
-    def start_conversation_callback(self, request, response):
-
-        chat = self.gpt_client.chat.completions.create(
-            model=self.MODEL,
-            messages=[self.get_role_message()],
-            temperature=0.7,
-        )
-        # TODO: Catch error from GPT response
-        response.chatgpt_response = chat.choices[0].message.content
-        response.success = True
-
-        return response
-
+        return chatgpt_persona_message
+    
 
 def main(args=None):
     rclpy.init(args=args)
