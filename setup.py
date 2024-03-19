ba40b71b19689a0767faa2383776b03f5b555294
Oliver Aberle <Oliver.Aberle@student.kit.edu>
2024-03-19 15:12:36 +0100

    Added package description

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     zip_safe=True,
     maintainer='maure',
     maintainer_email='romain.maure21@gmail.com',
-    description='TODO: Package description',
+    description='Package allowing any robot with a microphone to regonize speech',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
